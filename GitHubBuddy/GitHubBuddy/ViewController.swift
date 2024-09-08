a6d57366c01586209d6d141f2c533426901da5d9
Morley <codymorleyinc@gmail.com>
2024-09-07 18:14:56 -0700

    Opening project state.
    Project is ready for programmatic UI

---
+++ GitHubBuddy/GitHubBuddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubBuddy
+//
+//  Created by Cody Morley on 9/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
